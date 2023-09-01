f52c575839764bbcec20d6a6a2e69ccb9a1c3e62
kamal <kamal@gmail.com>
2023-09-01 13:44:31 +0000

    New Updates

--- myfirstpython.py
+++ myfirstpython.py
@@ -1,4 +1,8 @@
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
-for x in "banana":
+for x in "banana"
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
